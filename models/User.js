262f2cdecec927db4d0e37fa4395b819fc3ab8ed
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-22 14:23:29 +0530

    ..

--- models/User.js
+++ models/User.js
@@ -1,5 +1,51 @@
 const mongoose=require('mongoose')
 
 const userSchema=new mongoose.Schema({
-    
+    firstName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    accountType:{
+        type:String,
+        enum:["Admin","Student","Instructor"],
+        required:true
+    },
+    aditionalDetails:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"Profile"
+    },
+    courses:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Courses"
+        }
+    ],
+    image:{
+        type:String,
+        required:true
+    },
+    courseProgress:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"CourseProgress"
+        }
+    ],
 });
+
+module.exports=mongoose.model("User",userSchema);
