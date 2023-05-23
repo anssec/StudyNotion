8f0b42c86827ca38e8b4f8f220d579c28bec242e
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-23 15:52:38 +0530

    otpupdate

--- models/OTP.js
+++ models/OTP.js
@@ -1,4 +1,5 @@
-const mongoose=require('mongoose')
+const mongoose=require('mongoose');
+const mailSender = require('../utils/mailSender');
 
 const otpSchema=new mongoose.Schema({
 
@@ -15,6 +16,24 @@ const otpSchema=new mongoose.Schema({
         default:Date.now(),
         expires:5*60,
     },
+});
+
+//a function -> to send emails
+
+const sendVerificationEmail=async(email, otp)=>{
+    try {
+        const mailResponse=await mailSender(email,"Verification Email from StudyNotion",otp);
+        console.log("Email send successfully",mailResponse);
+
+    } catch (error) {
+        console.log('error occured while sending mails');
+        throw(error);
+    }
+};
+
+otpSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
 });
 
 module.exports=mongoose.model("OTP",otpSchema);
