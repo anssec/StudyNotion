e0f7e8698d340f2f61d26bd49d75813899b81fbf
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-26 18:04:48 +0530

    ..

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,27 +1,51 @@
-const User=require('../models/User');
-const OTP=require('../models/OTP');
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const otpGenerator = require("otp-generator");
 
 //* sendOtp
-exports.sendOTP=async(req,res)=>{
+exports.sendOTP = async (req, res) => {
+  try {
+    // fetch email from request body
+    const { email } = req.body;
 
-    //? fetch email from request body
-    const {email}=req.body;
+    // check if user is already exist
+    const checkUserPresent = await User.findOne({ email });
 
-    //? check if user is already exist
-    const checkUserPresent=await User.findOne({email});
+    //if user already exist , then return a response
+    if (checkUserPresent) {
+      return res.status(401).json({
+        success: false,
+        message: "user already registered",
+      });
+    }
 
-    //! if user already exist , then return a response
+    //? generate otp
+    var otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+    console.log("OTP generated: ", otp);
+
+    //check unique otp or not
+    const result = await OTP.findOne({ otp: otp });
+
+    while (result) {
+      otp = otpGenerator(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      });
+    }
+
     
+  } catch (error) {
 
-
-
-}
-
+  }
+};
 
 //sign up
 
-
 //log in
-
 
 //changePassword
