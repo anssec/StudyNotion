f9f7cdf0844addcbca534f8b9644b7bd094a5da5
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-11 20:05:29 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,51 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  accountType: {
+    type: String,
+    enum: ["Admin", "Student", "Instructor"],
+    require: true,
+  },
+  aditionalDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+    ref: "Profile",
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Courses",
+    },
+  ],
+  image: {
+    type: String,
+    require: true,
+  },
+  courseProgress: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CourseProgress",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
