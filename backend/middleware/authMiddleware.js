0402f1ce1785efc89f5d4d6d74d0ed67ac2c3af2
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-17 20:49:12 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -31,20 +31,61 @@ exports.auth = async (req, res, next) => {
     next();
   } catch (error) {
     return res.status(401).json({
-        success: false,
-        message: "Something went wrong while validating the token",
-      });
+      success: false,
+      message: "Something went wrong while validating the token",
+    });
   }
 };
 
 //isStudent
 exports.isStudent = async (req, res, next) => {
-    try {
-        
-    } catch (error) {
-        
+  try {
+    if (req.user.accountType !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "This is Protected routes for student only",
+      });
     }
-}
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "User role cannot be verified, please try again",
+    });
+  }
+};
 //isInstructor
+exports.isInstructor = async (req, res, next) => {
+  try {
+    if (req.user.accountType !== "Instructor") {
+      return res.status(401).json({
+        success: false,
+        message: "This is Protected routes for Instructor only",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "User role cannot be verified, please try again",
+    });
+  }
+};
 
 //isAdmin
+exports.isAdmin = async (req, res, next) => {
+    try {
+      if (req.user.accountType !== "Admin") {
+        return res.status(401).json({
+          success: false,
+          message: "This is Protected routes for Admin only",
+        });
+      }
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "User role cannot be verified, please try again",
+      });
+    }
+  };
