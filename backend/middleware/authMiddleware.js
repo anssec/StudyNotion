443792b905af97a0dee20fa9147cfa53931bbac2
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-17 20:38:26 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,10 +1,50 @@
-//auth
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/userModel");
 
+//auth
+exports.auth = async (req, res, next) => {
+  try {
+    //extract token
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorization").replace("Bearer ", "");
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token Is missing",
+      });
+    }
+
+    //verify the token
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decode;
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "Token Is Invalid",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(401).json({
+        success: false,
+        message: "Something went wrong while validating the token",
+      });
+  }
+};
 
 //isStudent
-
-
+exports.isStudent = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
 //isInstructor
-
 
 //isAdmin
