4b8fac34f93425f482e61fa5c6916fea27e20d61
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-17 22:21:41 +0530

    Update resetPasswordController.js

--- backend/controllers/resetPasswordController.js
+++ backend/controllers/resetPasswordController.js
@@ -1,5 +1,19 @@
-
+const User = require("../models/userModel");
+const mailsender = require("../utils/mailSender");
+require("dotenv").config();
 
 //resetPasswordToken
+exports.resetPasswordToken = async (req, res) => {
+  try {
+    //get email from req body
+    //check user for this email,email validation
+    //generate token
+    //update user by adding token and expiration time
+    //create url
+    //send mail containing url
+    //return response
 
+    const url = `${process.env.RESET_PASSWORD_FRONTEND_URL}/update-passsword/${token}`;
+  } catch (error) {}
+};
 //resetPassword
