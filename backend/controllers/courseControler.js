02e5ff050e3181a2a48af4244066dae8eaa61f66
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-20 23:10:52 +0530

    Create courseControler.js

---
+++ backend/controllers/courseControler.js
@@ -0,0 +1,19 @@
+const Course = require("../models/courseModel");
+const Tag = require("../models/tagsModel");
+const User = require("../models/userModel");
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+
+
+//createCourse handler function
+exports.createCourse=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+
+
+//getAllCourse handler function
