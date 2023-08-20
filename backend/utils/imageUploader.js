b6ac2457539ae03da5df74bfc92cf70af4b30a45
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-20 23:10:47 +0530

    Update imageUploader.js

--- backend/utils/imageUploader.js
+++ backend/utils/imageUploader.js
@@ -1,9 +1,14 @@
-const cloudinary=require("cloudinary")
+const cloudinary = require("cloudinary");
 
-exports.uploadImageToCloudinary=async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
+  const options = { folder };
+  if (height) {
+    options.height = height;
+  }
+  if (quality) {
+    options.quality = quality;
+  }
+  options.resource_type = "auto";
+
+  return await cloudinary.uploader.upload(file.tempFilePath, options);
+};
